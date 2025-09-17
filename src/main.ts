46fcf08a22645acf7697908c6e88dfe416955e2f
Sebastian-bear <21041277@itdurango.edu.mx>
2025-09-17 12:49:05 -0600

    Solución al scroll

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes'; // importa tus rutas reales
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top', // siempre sube al inicio
+        anchorScrolling: 'enabled'        // opcional, si usas #anclas
+      })
+    ),
+  ],
+}).catch((err) => console.error(err));
